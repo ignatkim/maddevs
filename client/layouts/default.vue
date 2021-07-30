be78cfb8d60f802cb1128a852923cbb66bd6c685
Kirill kotyunin <kotyunin.k@gmail.com>
2021-07-30 08:12:44 +0600

    MAR-719 Added middleware for handle prismic api errors

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -23,6 +23,22 @@ export default {
     Footer,
   },
 
+  /**
+   * This middleware is needed to update the masterRef prismic parameter if it has expired
+   * More about this issue - https://community.prismic.io/t/expired-token-access-token-403-error-on-some-page-visits-safari/4369/23
+   * Solution - https://community.prismic.io/t/expired-token-access-token-403-error-on-some-page-visits-safari/4369/26
+   */
+  async middleware({ route = { name: '' }, $axios, $prismic }) {
+    const pagesWithPrismic = ['careers', 'blog', 'ebooks', 'customer-university']
+
+    if (pagesWithPrismic.some(page => route.name.includes(page))) {
+      const prismicData = await $axios.get('https://SuperPuperTest.cdn.prismic.io/api/v2')
+      const currentMasterRef = $prismic.api.masterRef.ref
+      const freshMasterRef = prismicData.data.refs[0].ref
+      if (process.client && currentMasterRef !== freshMasterRef) $prismic.api.masterRef.ref = freshMasterRef
+    }
+  },
+
   mounted() {
     this.initHashLinks()
     this.loadIntercomScript()
