a0f44bc0c2dd0975fe67a5ca4ceb93525acf095d
Denisoed <denisoed@gmail.com>
2021-08-03 11:09:54 +0600

    MAR-1516 changed styles for key metrics section

--- client/components/Careers/KeyMetrics.vue
+++ client/components/Careers/KeyMetrics.vue
@@ -37,19 +37,28 @@ export default {
 
 <style lang="scss" scoped>
 .key-metrics {
-  &_container,
+  &_container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 151px;
+  }
+
   &_grid {
     display: grid;
-  }
-
-  &_container {
-    margin-bottom: 101px;
-    grid-template-columns: 1fr 2fr;
-  }
-
-  &_grid {
     grid-template-columns: repeat(3, 1fr);
-    grid-gap: 20px;
+    grid-gap: 30px;
+    margin-top: 37px;
+
+    /deep/ .metrica-card {
+      background-color: #1D1D1F;
+      padding: 22px 47px 30px;
+
+      &_title,
+      &_description {
+        color: #CFCFD0;
+      }
+    }
 
     @media screen and (max-width: 630px) {
       grid-template-columns: repeat(2, 1fr);
@@ -61,25 +70,15 @@ export default {
   }
 
   &_title {
-    margin-right: 48px;
     @include font('Poppins', 60px, 700);
     letter-spacing: -2.6px;
-    line-height: 64px;
+    line-height: 67px;
     color: $text-color--white;
   }
 
   @media screen and (max-width: 1340px) {
     &_container {
       grid-template-columns: 1fr;
-    }
-
-    &_grid {
-      grid-gap: 10px;
-    }
-
-    &_title {
-      margin-right: 0;
-      margin-bottom: 27px;
     }
   }
 
@@ -88,10 +87,30 @@ export default {
       margin-bottom: 65px;
     }
 
+    &_grid {
+      grid-gap: 10px;
+
+      /deep/ .metrica-card {
+        padding: 16px;
+      }
+    }
+
     &_title {
       margin-bottom: 21px;
       font-size: 40px;
       line-height: 50px;
+      letter-spacing: -0.04em;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    &_grid {
+      margin-top: 21px;
+    }
+
+    &_title {
+      font-size: 34px;
+      line-height: 40px;
       letter-spacing: -0.04em;
     }
   }
