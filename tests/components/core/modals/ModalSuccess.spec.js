30bb6b2cee2f629a4424c5eb353373adf2425fc5
Roman Chasovitin <chasovitin.r@gmail.com>
2021-08-19 16:59:10 +0600

    MAR-1787. Updated unit-tests.

--- tests/components/core/modals/ModalSuccess.spec.js
+++ tests/components/core/modals/ModalSuccess.spec.js
@@ -1,6 +1,6 @@
-import { render, screen, fireEvent } from '@testing-library/vue'
+import { render } from '@testing-library/vue'
+import { mount } from '@vue/test-utils'
 import ModalSuccess from '@/components/core/modals/ModalSuccess'
-import delay from '../../../../client/helpers/delay'
 
 const props = {
   visibled: true,
@@ -8,56 +8,35 @@ const props = {
   displayTime: 1000,
 }
 
+const $refs = {
+  successModal: {
+    makeSuccess: jest.fn(),
+    show: jest.fn(),
+    close: jest.fn(),
+  },
+}
+
 describe('ModalSuccess component', () => {
   it('should render correctly', () => {
     const { container } = render(ModalSuccess, {
       props,
+      $refs,
     })
 
     expect(container).toMatchSnapshot()
   })
 
-  it('should work close modal method', async () => {
-    const { emitted } = render(ModalSuccess, {
+  it('should modal correctly open and close', async () => {
+    const wrapper = mount(ModalSuccess, {
       props,
     })
 
-    fireEvent.click(screen.queryByTestId('test-close-modal'))
+    wrapper.vm.show()
 
-    expect(emitted().close).toHaveLength(1)
-  })
+    expect(wrapper.findComponent({ name: 'SuccessMessage' })).toBeTruthy()
 
-  it('should correct work auto close handler', async () => {
-    props.visibled = false
-    const { emitted, updateProps } = render(ModalSuccess, {
-      props,
-    })
+    wrapper.vm.close()
 
-    await updateProps({
-      visibled: true,
-      id: 'modal-id',
-      displayTime: 1000,
-    })
-
-    await delay(1500)
-
-    expect(emitted().close).toHaveLength(1)
-  })
-
-  it('the timer should be reset if the modal was closed early', async () => {
-    props.visibled = true
-    const { emitted, updateProps } = render(ModalSuccess, {
-      props,
-    })
-
-    await updateProps({
-      visibled: false,
-      id: 'modal-id',
-      displayTime: 1000,
-    })
-
-    await delay(1500)
-
-    expect(!!emitted().close).toBeFalsy()
+    expect(wrapper).toMatchSnapshot()
   })
 })
