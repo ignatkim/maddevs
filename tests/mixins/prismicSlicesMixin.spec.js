5f19edca922e22e07a185278622438bec948f04f
Kirill kotyunin <kotyunin.k@gmail.com>
2021-08-20 15:12:47 +0600

    MAR-1543 Fixed unit-tests

--- tests/mixins/prismicSlicesMixin.spec.js
+++ tests/mixins/prismicSlicesMixin.spec.js
@@ -3,7 +3,7 @@ import prismicSlicesMixin from '@/mixins/prismicSlicesMixin'
 describe('Prismic Slices Mixin', () => {
   describe('formatHtmlText method', () => {
     it('should return empty string with incorrect parameter', () => {
-      expect(prismicSlicesMixin.methods.formatHtmlText(undefined)).toBe('')
+      expect(prismicSlicesMixin.methods.formatHtmlText(undefined)).toBeUndefined()
     })
 
     it('should remove the &#8232; unicode of \'L SEP\' symbol from string', () => {
