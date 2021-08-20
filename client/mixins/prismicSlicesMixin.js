6ca9c17c3775c1ea27e8b6cef268418747fe0116
Kirill kotyunin <kotyunin.k@gmail.com>
2021-08-20 15:03:35 +0600

    MAR-1543 Corrected condition of formatHtmlText function

--- client/mixins/prismicSlicesMixin.js
+++ client/mixins/prismicSlicesMixin.js
@@ -1,7 +1,7 @@
 const prismicSlicesMixin = {
   methods: {
     formatHtmlText(text) {
-      if (typeof text !== 'string') return ''
+      if (typeof text !== 'string') return text
       return text
         .replace(/&#8232;/g, '') // removes the "L SEP" symbol from text
     },
