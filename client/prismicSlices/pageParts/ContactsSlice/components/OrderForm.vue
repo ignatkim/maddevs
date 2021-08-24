400ef94ef19abc722b58b92b549d3459f2f4daeb
Kirill kotyunin <kotyunin.k@gmail.com>
2021-08-24 16:18:00 +0600

    MAR-1789 Corrected styles for the ContactsSlice form

--- client/prismicSlices/pageParts/ContactsSlice/components/OrderForm.vue
+++ client/prismicSlices/pageParts/ContactsSlice/components/OrderForm.vue
@@ -47,6 +47,7 @@ export default {
   @media screen and (max-width: 440px) {
     padding: 35px 12px;
   }
+
   &__title {
     @include font('Inter', 42px, 700);
     line-height: 46px;
@@ -66,92 +67,94 @@ export default {
     }
   }
 
-  /deep/ &__button {
-    font-size: 16px;
-    line-height: 24px;
-    font-weight: 400;
-    color: $text-color--black-lighter;
-    padding: 18px;
-    background-color: transparent;
-    border: 1px solid $border-color--red-opacity;
-  }
-
-  /deep/ .entry-field {
-    color: #848484;
-    border: 1px solid $border-color--grey-form;
-    background: transparent;
-    padding: 13px 20px;
-    border-radius: 6px;
-    &::placeholder {
+  .form {
+    /deep/ .entry-field {
       color: #848484;
+      border: 1px solid $border-color--grey-form;
+      background: transparent;
+      padding: 13px 20px;
+      border-radius: 6px;
+      &::placeholder {
+        color: #848484;
+      }
+      @media screen and (max-width: 1024px) {
+        font-size: 14px;
+      }
     }
-    @media screen and (max-width: 1024px) {
-      font-size: 14px;
-    }
-  }
 
-  /deep/ .error-text {
-    @media screen and (max-width: 1024px) {
-      font-size: 12px;
+    /deep/ .error-text {
+      @media screen and (max-width: 1024px) {
+        font-size: 12px;
+      }
+      @media screen and (max-width: 768px) {
+        font-size: 14px;
+      }
+      @media screen and (max-width: 600px) {
+        font-size: 12px;
+      }
     }
-    @media screen and (max-width: 768px) {
-      font-size: 14px;
-    }
-    @media screen and (max-width: 600px) {
-      font-size: 12px;
-    }
-  }
 
-  /deep/ .v-placeholder-asterisk {
-    @media screen and (max-width: 1024px) {
-      font-size: 14px;
+    /deep/ .v-placeholder-asterisk {
+      @media screen and (max-width: 1024px) {
+        font-size: 14px;
+      }
     }
-  }
 
-  /deep/ .field-item {
-    margin-bottom: 12px;
-    &:nth-child(2),
-    &:nth-child(3) {
-      display: inline-block;
-      width: calc(50% - 7px);
-      vertical-align: top;
+    /deep/ .field-item {
+      margin-bottom: 12px;
+      &:nth-child(2),
+      &:nth-child(3) {
+        display: inline-block;
+        width: calc(50% - 7px);
+        vertical-align: top;
+      }
+      &:nth-child(2) {
+        margin-right: 9.5px;
+      }
+      &:last-child {
+        margin-bottom: 0;
+      }
+      @media screen and (max-width: 1024px) {
+        margin-bottom: 10px;
+      }
     }
-    &:nth-child(2) {
-      margin-right: 9.5px;
-    }
-    &:last-child {
-      margin-bottom: 0;
-    }
-    @media screen and (max-width: 1024px) {
-      margin-bottom: 10px;
-    }
-  }
 
-  /deep/ .textarea {
-    height: 192px;
-    @media screen and (max-width: 1024px) {
-      height: 92px;
-    }
-    @media screen and (max-width: 768px) {
+    /deep/ .textarea {
       height: 192px;
+      @media screen and (max-width: 1024px) {
+        height: 92px;
+      }
+      @media screen and (max-width: 768px) {
+        height: 192px;
+      }
+      @media screen and (max-width: 600px) {
+        height: 92px;
+      }
     }
-    @media screen and (max-width: 600px) {
-      height: 92px;
-    }
-  }
 
-  /deep/ .form-checkboxes {
-    margin: 18px 0 46px;
-    @media screen and (max-width: 1024px) {
-      margin: 18px 0 30px;
+    /deep/ .form-checkboxes {
+      margin: 18px 0 46px;
+      @media screen and (max-width: 1024px) {
+        margin: 18px 0 30px;
+      }
     }
-  }
 
-  /deep/ .form-checkbox-label {
-    font-size: 16px;
-    line-height: 24px;
-    @media screen and (max-width: 1024px) {
-      font-size: 14px;
+    /deep/ .form-checkbox-label {
+      font-size: 16px;
+      line-height: 24px;
+      @media screen and (max-width: 1024px) {
+        font-size: 14px;
+      }
+    }
+
+    /deep/ .contacts-slice-form__button {
+      font-size: 16px;
+      line-height: 24px;
+      font-weight: 400;
+      color: $text-color--black-lighter;
+      padding: 18px;
+      background-color: transparent;
+      border: 1px solid $border-color--red-opacity;
     }
   }
 }
