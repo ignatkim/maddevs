9afc08315fcf220b7884b21959e8f03d71c0f5d2
TangoPJ <livechess@yahoo.com>
2021-08-25 13:51:04 +0600

    MAR-1814. Сondition was corrected

--- client/api/ipInfo.js
+++ client/api/ipInfo.js
@@ -3,9 +3,8 @@ import axios from 'axios'
 export const getIPInfo = async () => {
   try {
     let response = await axios.get('https://ipapi.co/json')
-    if (response.data.ip) {
-      return response.data
-    }
+    if (response.data.ip) return response.data
+
     response = await axios.get('http://ip-api.com/json')
     response.data.ip = response.data.query
     return response.data
