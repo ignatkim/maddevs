0e545335da0bffd717494c497c6747a8fab539e9
TangoPJ <rail@maddevs.io>
2021-09-01 10:45:39 +0000

    MAR-1403. Deleted dependecies from img-comparison-slider file

--- client/plugins/img-comparison-slider.js
+++ client/plugins/img-comparison-slider.js
@@ -1,9 +1,2 @@
-import Vue from 'vue'
-
 // Import the library
 import 'img-comparison-slider'
-
-Vue.config.productionTip = false
-
-// Tell Vue that the web component is present.
-Vue.config.ignoredElements = [/img-comparison-slider/]
