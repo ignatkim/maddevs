a11eba78ca3adb268d87a56f9a5e7f87f06ac7fd
Kirill kotyunin <kotyunin.k@gmail.com>
2021-09-03 13:37:01 +0600

    MAR-1799 Updated unit-tests of the StartScreen slice

--- tests/prismicSlices/pageParts/StartScreen/index.spec.js
+++ tests/prismicSlices/pageParts/StartScreen/index.spec.js
@@ -1,17 +1,18 @@
+import { shallowMount } from '@vue/test-utils'
 import { render, screen } from '@testing-library/vue'
 import StartScreen from '@/prismicSlices/pageParts/StartScreen'
 
-// const background = {
-//   white: '#fff',
-//   black: '#111213',
-//   grey: '#f5f7f9',
-// }
+const backgrounds = {
+  white: '#fff',
+  black: '#111213',
+  grey: '#f5f7f9',
+}
 
-// const gradient = {
-//   black: 'linear-gradient(180deg, rgba(17, 18, 19, 0) 60%, #111213)',
-//   white: 'linear-gradient(180deg, rgba(17, 18, 19, 0) 60%, #fff)',
-//   grey: 'linear-gradient(180deg, rgba(17, 18, 19, 0) 60%, #f5f7f9)',
-// }
+const gradients = {
+  black: 'linear-gradient(180deg, rgba(17, 18, 19, 0) 60%, #111213)',
+  white: 'linear-gradient(180deg, rgba(17, 18, 19, 0) 60%, #fff)',
+  grey: 'linear-gradient(180deg, rgba(17, 18, 19, 0) 60%, #f5f7f9)',
+}
 
 const apiData = {
   background: 'black',
@@ -52,5 +53,97 @@ describe('StartScreen slice', () => {
     expect(screen.queryByTestId('start-screen-slice__title')).not.toBeNull()
     expect(screen.queryByTestId('start-screen-slice__subtitle')).toBeNull()
     expect(container).toMatchSnapshot()
+  })
+
+  describe('sliceBackground computed method', () => {
+    it('should return #fff (white)', () => {
+      const wrapper = shallowMount(StartScreen, {
+        propsData: getProps({
+          ...apiData,
+          background: 'white',
+        }),
+      })
+
+      expect(wrapper.vm.sliceBackground).toBe(backgrounds.white)
+    })
+
+    it('should return #f5f7f9 (grey)', () => {
+      const wrapper = shallowMount(StartScreen, {
+        propsData: getProps({
+          ...apiData,
+          background: 'grey',
+        }),
+      })
+
+      expect(wrapper.vm.sliceBackground).toBe(backgrounds.grey)
+    })
+
+    it('should return #111213 (black)', () => {
+      const wrapper = shallowMount(StartScreen, {
+        propsData: getProps({
+          ...apiData,
+          background: 'black',
+        }),
+      })
+
+      expect(wrapper.vm.sliceBackground).toBe(backgrounds.black)
+    })
+
+    it('should return null', () => {
+      const wrapper = shallowMount(StartScreen, {
+        propsData: getProps({
+          ...apiData,
+          background: 'unknown',
+        }),
+      })
+
+      expect(wrapper.vm.sliceBackground).toBeNull()
+    })
+  })
+
+  describe('sliceGradient computed method', () => {
+    it('should return linear-gradient(180deg, rgba(17, 18, 19, 0) 60%, #fff) (white gradient)', () => {
+      const wrapper = shallowMount(StartScreen, {
+        propsData: getProps({
+          ...apiData,
+          gradientColor: 'white',
+        }),
+      })
+
+      expect(wrapper.vm.sliceGradient).toBe(gradients.white)
+    })
+
+    it('should return #f5f7f9 (grey gradient)', () => {
+      const wrapper = shallowMount(StartScreen, {
+        propsData: getProps({
+          ...apiData,
+          gradientColor: 'grey',
+        }),
+      })
+
+      expect(wrapper.vm.sliceGradient).toBe(gradients.grey)
+    })
+
+    it('should return #111213 (black gradient)', () => {
+      const wrapper = shallowMount(StartScreen, {
+        propsData: getProps({
+          ...apiData,
+          gradientColor: 'black',
+        }),
+      })
+
+      expect(wrapper.vm.sliceGradient).toBe(gradients.black)
+    })
+
+    it('should return null', () => {
+      const wrapper = shallowMount(StartScreen, {
+        propsData: getProps({
+          ...apiData,
+          gradientColor: 'unknown',
+        }),
+      })
+
+      expect(wrapper.vm.sliceGradient).toBeNull()
+    })
   })
 })
